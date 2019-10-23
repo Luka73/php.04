818e2ee7b7f7525919cf620ab057d9f206b403b6
Luana <luanafernandes73@gmail.com>
2019-10-23 20:42:19 -0300

    Commit inicial

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <title>Document</title>
+</head>
+<body>
+    <div class="container">
+       <div class="col-4">
+            <h2>Área Restrita</h2>
+            <form action="">
+                <label for="">Login: </label>
+                <input type="text" class="form-control">
+                <label for="">Senha: </label>
+                <input type="password" class="form-control">
+                <input type="submit" value="Entrar" class="btn btn-success mt-2">
+            </form>
+        </div>
+    </div>
+</body>
+</html>
